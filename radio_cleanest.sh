23a47a6c461c0b0429aab3afc608b4e9faf9bea1
Alex Volkov <alex@flamy.ca>
2016-09-05 16:18:30 -0400

    Squelch is redundant

--- radio_cleanest.sh
+++ radio_cleanest.sh
@@ -3,7 +3,7 @@ rmmod dvb_usb_rtl28xxu rtl2832
 output="output/test_"
 freq=94.1
 rm ${output}${freq}.ogv
-rtl_fm -f ${freq}M -M fm -s 200k -A std -l0  -E deem -r 44.1k - |  avconv -f s16le -ar 44100 -i pipe:0  -acodec libvorbis -qscale:a 7  "${output}${freq}.ogv" &
+rtl_fm -f ${freq}M -M fm -s 200k -A std -E deem -r 44.1k - |  avconv -f s16le -ar 44100 -i pipe:0  -acodec libvorbis -qscale:a 7  "${output}${freq}.ogv" &
 sleep 60
 kill $(ps aux | grep rtl_fm | grep deem | awk '{print $2}')
 
