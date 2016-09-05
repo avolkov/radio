f5794de5a7147ff64457d91989c2033103433ccd
Alex Volkov <alex@flamy.ca>
2016-09-05 17:32:16 -0400

    Cleanups.

--- play_radio.sh
+++ play_radio.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rmmod dvb_usb_rtl28xxu rtl2832
-rtl_fm -f 94.1e6 -M wbfm -s 200000 -r 44100 - | aplay -r 44100 -f S16_LE
-#rtl_fm -f 94.1e6 -M wbfm -s 200000 -r 48000 - | aplay -r 48000 -f S16_LE
+freq=94.1
+rtl_fm -f ${freq}M -M fm -s 200k -A std -E deem -r 44.1k - |  aplay -r 44100 -f S16_LE 
 
