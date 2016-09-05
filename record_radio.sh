77cbc031dac3daf65b41c8a69689aec84169c009
Alex Volkov <alex@flamy.ca>
2016-09-05 15:14:34 -0400

    Expanded rtl_fm options

--- record_radio.sh
+++ record_radio.sh
@@ -2,4 +2,4 @@
 rmmod dvb_usb_rtl28xxu rtl2832
 output="output/test.ogv"
 rm "${output}"
-rtl_fm -f 94.1e6 -M wbfm -s 200000 -r 44100 - |  avconv -f s16le -ar 44100 -ac 1 -i pipe:0  -acodec libvorbis -qscale:a 5  ${output}
+rtl_fm -f 94.1e6 -M fm -s 200k -A fast -l 0 -E deem -r 44.1k - |  avconv -f s16le -ar 44100 -ac 1 -i pipe:0  -acodec libvorbis -qscale:a 5  ${output}
