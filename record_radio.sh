a689a633822a1d368c1b52c5002c0134edb8122b
Alex Volkov <alex@flamy.ca>
2016-09-05 15:06:36 -0400

    fixing output

--- record_radio.sh
+++ record_radio.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 rmmod dvb_usb_rtl28xxu rtl2832
-rtl_fm -f 94.1e6 -M wbfm -s 200000 -r 44100 - |  avconv -f s16le -ar 44100 -ac 1 -i pipe:0  -acodec libvorbis -qscale:a 5  test.ogv
+output="output/test.ogv"
+rm "${output}"
+rtl_fm -f 94.1e6 -M wbfm -s 200000 -r 44100 - |  avconv -f s16le -ar 44100 -ac 1 -i pipe:0  -acodec libvorbis -qscale:a 5  ${output}
